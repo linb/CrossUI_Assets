f7133374376bbdfb5d1f20bc16031da644a12885
Jack Li <linb.net@gmail.com>
2020-11-05 19:02:47 -0500

    Update Module.js

--- React/assets/pages/Module.js
+++ React/assets/pages/Module.js
@@ -1,5 +1,5 @@
 /*jshint esversion: 8 */
-import { React, html } from "../../web_modules/preact-htm/index.js"; 
+import { React, html } from "../web_modules/preact-htm/index.js"; 
 
 const Module = props => {
     const [defaultState, setDefaultState] = React.useState({ });
