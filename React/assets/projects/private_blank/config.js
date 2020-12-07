ca9c92f47a9879080a1cf9d59a822851402650e2
Jack Li <linb.net@gmail.com>
2020-12-07 16:06:10 -0500

    Update config.js

--- React/assets/projects/private_blank/config.js
+++ React/assets/projects/private_blank/config.js
@@ -1,3 +1,30 @@
+/*jshint esversion: 8 */
 export default {
-  baseUrl:"/"  
+    baseUrl : "",
+    mockFetch : (options) =>{
+        let data;
+        const mock = (options) => {
+            switch(options.url){
+                case "checkToken":
+                    data = { token : "fake_token" };
+                    break;
+                case "signUp":
+                    data = { user:{ame:"User",token:"fake_token"}};
+                    break;
+                case "signIn":
+                    data = { user:{ame:"User",token:"fake_token"}};
+                    break;
+                case "signOut":
+                    data = {  };
+                    break;
+            }
+            return Object.assign({ok : 1}, data);
+        };
+
+        return new Promise((resolve, reject) => {
+            setTimeout(() => {
+                resolve(mock(options));
+            }, 300);
+        });        
+    }
 };
