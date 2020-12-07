f00b9e15d48bf2a147af06f0bac6dd5334e15037
Jack Li <linb.net@gmail.com>
2020-12-07 16:02:56 -0500

    Update auth.js

--- React/assets/projects/private_blank/pages/components/auth.js
+++ React/assets/projects/private_blank/pages/components/auth.js
@@ -1,24 +1,51 @@
 /*jshint esversion: 8 */
+import CONF from "../../config.js";
 import {utils} from "../../web_modules/react-hook-module/index.js";
+import {axios} from "../../web_modules/react-hook-module/plugin_request.js";
 
-const signIn = function(){
-  const auth = this;
-  const user = {email:"fake@email.com"};
-  setTimeout(() => {
-    auth.setUser(user);
-    utils.setCookie("user", user);
-  }, 500);
+// default status - need checking: user/token is false
+// nologin status: user/token is null
+// login status: user/token is not null/undefiend/empty
+
+const fetchTool = CONF && CONF.mockFetch ? { request : CONF.mockFetch } : axios;
+
+const signIn = function(params){
+    const auth = this;
+    const rsp = {token:'token', name:'name'};
+
+    auth.setUser(rsp.user);
+    auth.setToken(rsp.user.token);
+
+    utils.setCookie("user", rsp.user);
+    utils.setCookie("token", rsp.user.token);
 };
-const signOut = function(){
-  this.setUser(null);
-  utils.removeCookie("user");
+const signUp = function(params){
+    const auth = this;
+    const rsp = {token:'token', name:'name'};
+
+    auth.setUser(rsp.user);
+    auth.setToken(rsp.user.token);
+
+    utils.setCookie("user", rsp.user);
+    utils.setCookie("token", rsp.user.token);
+};
+
+const signOut = function(token){
+    const auth = this;
+    auth.setUser(null);
+    auth.setToken(null);
+
+    utils.removeCookie("user");
+    utils.removeCookie("token");
 };
 const authInit = function(){
-  const auth = this;
-  const user = utils.getCookie("user");
-  user && auth.setUser(user);
-  return function(){
-    // nothing
-  };
+    const auth = this;
+    // to init things
+    return function(){
+        // to clear things
+    };
 };
-export default {signIn, signOut, authInit};
+const user = utils.getCookie("user") || false;
+const token = user && user.token || false;
+
+export default {signIn, signUp, user, token, signOut, authInit};
