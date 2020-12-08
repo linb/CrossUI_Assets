559c4a4776e53e32f20cbbd119140ea022749e92
Jack Li <linb.net@gmail.com>
2020-12-08 02:01:04 -0500

    Update index.js

--- React/assets/projects/DashBoards/xui_project_tpl_DashBoard1/index.js
+++ React/assets/projects/DashBoards/xui_project_tpl_DashBoard1/index.js
@@ -3,17 +3,17 @@ import { React, html } from "./web_modules/preact-htm/index.js";
 import { ReactDOM } from "./web_modules/preact-htm/index.js";
 import { BrowserRouter, HashRouter as Router, RelativeRouter } from "./web_modules/react-hook-module/plugin_router.js";
 import { ProvideAuth } from "./web_modules/react-hook-module/plugin_auth.js";
-import { setGlobalBaseURL, setMockFetch } from "./web_modules/react-hook-module/plugin_request.js";
+import { setGlobalBaseURL, setFetchMocker } from "./web_modules/react-hook-module/plugin_request.js";
 import CONF from "./config.js";
 
 import App from "./pages/App.js";
 import configure from "./pages/components/auth.js";
 
-if(CONF.baseUrl){
-    setGlobalBaseURL(CONF.baseUrl);
+if(CONF.baseURL){
+    setGlobalBaseURL(CONF.baseURL);
 }
-if(CONF.mockFetch){
-    setMockFetch(CONF.mockFetch);
+if(CONF.fetchMocker){
+    setFetchMocker(CONF.fetchMocker);
 }
 
 ReactDOM.render( html`
